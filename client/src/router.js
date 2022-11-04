4e2abfa2f904f357705339592218213aaf084d4f
joicenunes <anunes.joice@gmail.com>
2022-11-04 14:08:42 -0300

    Componentes e roteamento para registro, confirmação + configuração de subpáginas para startup

--- client/src/router.js
+++ client/src/router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Logon from './Pages/Logon';
+import { Logon, Register, Confirmation } from './Pages/Access';
 import Startup from './Pages/Startup';
 import Vitrine from './Pages/Vitrine'
 
@@ -12,7 +12,12 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path='/' exact element={<Logon />} />
-        <Route path='/startup' element={<Startup />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/confirmation' element={<Confirmation />} />
+        <Route path='/startup' element={<Startup />}>
+          <Route path='' element={<Startup />} />
+          <Route path=':id' element={<Startup />} />
+        </Route>
         <Route path='/vitrine' element={<Vitrine />} />
       </Routes>
     </BrowserRouter>
