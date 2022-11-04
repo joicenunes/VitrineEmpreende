40b948576b866f290ff66708c9d95773cc4ae751
joicenunes <anunes.joice@gmail.com>
2022-11-04 14:11:43 -0300

    Descrição de startup on hover

--- client/src/Components/StartupProfile/index.js
+++ client/src/Components/StartupProfile/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
@@ -7,10 +7,9 @@ import { RiLinkedinBoxLine, RiTwitterLine, RiGithubLine } from 'react-icons/ri';
 
 import { Card, CardBody } from '../../Components/Card';
 import TagList from '../../Components/TagList';
-import Button from '../Button';
+// import Button from '../Button';
 
 export default function StartupProfile(props) {
-  const [isDescriptionVisible, setDescriptionVisibility] = useState(false);
 
   const setTitleColor = (color) => {
     switch (color) {
@@ -28,28 +27,6 @@ export default function StartupProfile(props) {
       </div>
   }
 
-  const showButtons = () => {
-    return props.hiddenButtons ? <></> :
-      <div className='startup-actions'>
-        {
-          props.hiddenDescription ??
-          <Button
-            color='primary'
-            onClickButton={toggleDescriptionVisibility}
-          >
-            Resumo
-          </Button>
-        }
-        <Link to={`/startup/${props.startup.id}`}>
-          <Button
-            color='primary'
-          >
-            Ir para
-          </Button>
-        </Link>
-      </div>
-  }
-
   const showTags = (tags) => {
     return props.hiddenTags ? <></> :
       <div className='startup-tags'>
@@ -61,45 +38,42 @@ export default function StartupProfile(props) {
     return startup.img ?? { backgroundImage: `url(${startup.img})` }
   }
 
-  const toggleDescriptionVisibility = () => {
-    !props.hiddenDescription && setDescriptionVisibility(!isDescriptionVisible);
-  }
-
   return (
     <div className='startup-profile'>
-      <Card>
-        <CardBody>
-          <div
-            className='startup-avatar'
-            style={showStartupPhoto(props.startup)}
-          >
-            {
-              props.startup.img ??
-              <MdOutlinePhoto
-                size={100}
-              />
-            }
-          </div>
-          <div className='startup-name'>
-            {props.startup.name}
-          </div>
-          <div className='startup-contact'>
-            {props.startup.email}
-          </div>
-          {showSocialNetwork()}
-          {showTags(props.startup.tags)}
-          {showButtons()}
-        </CardBody>
-      </Card>
+      <Link
+        to={`/startup/${props.startup.id}`}
+        style={{ pointerEvents: props.noLink ? 'none' : '' }}
+      >
+        <Card>
+          <CardBody>
+            <div
+              className='startup-avatar'
+              style={showStartupPhoto(props.startup)}
+            >
+              {
+                props.startup.img ??
+                <MdOutlinePhoto
+                  size={100}
+                />
+              }
+            </div>
+            <div className='startup-name'>
+              {props.startup.name}
+            </div>
+            <div className='startup-contact'>
+              {props.startup.email}
+            </div>
+            {showSocialNetwork()}
+            {showTags(props.startup.tags)}
+          </CardBody>
+        </Card>
 
-      {
-        !props.hiddenDescription && isDescriptionVisible ?
-          <div
-            className='startup-description'
-          >
-            <p>{props.startup.description}</p>
-          </div> : <></>
-      }
+        <div
+          className={`startup-description${props.noLink ? '-disabled' : ''}`}
+        >
+          <p>{props.startup.description}</p>
+        </div>
+      </Link>
     </div>
   );
 }
