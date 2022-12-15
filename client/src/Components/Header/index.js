0d2a7c4cb5e3e93ef29b316cb2b820d9630b547b
joicenunes <anunes.joice@gmail.com>
2022-12-15 15:00:18 -0300

    [HEADER] - Link para página de login ou para startup do usuário

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 
 import Avatar from '../Avatar';
 
 export default function Header(props) {
+  let [logged, /* setLogged */] = useState(localStorage.getItem('userId') !== null);
+  let [userName, /* setUserName */] = useState(localStorage.getItem('userName'));
+  let [startupId, /* setStartupId */] = useState(localStorage.getItem('startupId'));
+
   return (
     <header>
       <div className='col'>
@@ -14,18 +18,23 @@ export default function Header(props) {
       </div>
       <div className='col'></div>
       <div className='col right'>
-        {/* <div className='actions'> */}
-          <div className='search-startup'>
-            <Link to='/vitrine'>Buscar startup</Link>
-          </div>
+        <div className='search-startup'>
+          <Link to='/vitrine'>Buscar startup</Link>
         </div>
-        <div className='col right'>
-          <div className='user-avatar'>
+      </div>
+      <div className='col right'>
+        <div className='user-avatar'>
+          {
+            logged ?
+            <Link to={`/startup/${startupId}`}>
+              <Avatar name={userName} />
+            </Link> :
             <Link to='/startup'>
-              <Avatar name={'Joice Nunes'} />
+              Entrar
             </Link>
-          </div>
-        {/* </div> */}
+          }
+          
+        </div>
       </div>
     </header>
   );
