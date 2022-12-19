dc023c1733a3c5341cadeeeee6764df753ba8551
henriquej0 <m.henriquej0@gmail.com>
2022-12-19 11:02:38 -0300

    Mudanças no front
    - adição à alguns componentes
    - adição de assets e cons

--- client/src/Components/Button/index.js
+++ client/src/Components/Button/index.js
@@ -2,40 +2,41 @@ import React from 'react';
 import colorsMap from '../../Helper/colors';
 
 export default function Button(props) {
-  const setButtonStyle = () => {
-    let style = {};
+    const setButtonStyle = () => {
+        let style = {};
 
-    if (props.block) {
-      style.width = '100%';
-    }
-    if (props.outline) {
-      style.backgroundColor = 'transparent';
-      style.borderStyle = 'solid';
-      style.borderWidth = '1px';
-      if (props.color) {
-        style.color = colorsMap[props.color];
-        style.borderColor = colorsMap[props.color];
-      }
-    } else {
-      if (props.color) {
-        style.color = '#fff';
-        style.backgroundColor = colorsMap[props.color];
-      }
-    }
+        if (props.block) {
+            style.width = '100%';
+            style.borderRadius = '8px';
+        }
+        if (props.outline) {
+            style.backgroundColor = 'transparent';
+            style.borderStyle = 'solid';
+            style.borderWidth = '1px';
+            style.borderRadius = '8px';
 
-    return style;
-  }
-  return (
-    <div className='form-button'>
-      <button
-        className='button'
-        style={setButtonStyle()}
-        onClick={props.onClickButton}
-        type='submit'
-        disabled={props.disabled}
-      >
-        { props.children }
-      </button>
-    </div>
-  );
+            if (props.color) {
+                style.color = colorsMap[props.color];
+                style.borderColor = colorsMap[props.color];
+            }
+        } else {
+            if (props.color) {
+                style.color = '#fff';
+                style.backgroundColor = colorsMap[props.color];
+            }
+        }
+
+        return style;
+    }
+    return ( <
+        div className = 'form-button' >
+        <
+        button className = 'button'
+        style = { setButtonStyle() }
+        onClick = { props.onClickButton }
+        type = 'submit'
+        disabled = { props.disabled } > { props.children } <
+        /button>  <
+        /div>
+    );
 }
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -2,35 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import empreendeImg from '../../Assets/ft_condominio.png';
+import portIcon from '../../Assets/icon_portfolio.png';
+import empIcon from '../../Assets/icon_empreende.png';
 
 import '../Access/style.css';
 import Button from '../../Components/Button';
 
 export default function Home() {
-  const imageConfig = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${empreendeImg})`;
+    const imageConfig = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${empreendeImg})`;
 
-  return (
-    <div className='access'>
-      <div
-        className='access-image'
-        style={{ backgroundImage: imageConfig }}
-        alt='Empreende UFC'
-      >
-        <div className='access-image-title'>
-          Portfólio de Startups
-        </div>
-      </div>
-      <div className='access-form'>
-        {/* <img src={logo} className='logo' alt='logo' /> */}
-        <h1>Bem-vindo(a)!</h1>
-        <p>O Portfólio de Startups é uma iniciativa do Condomínio de Empreendedorismo da Universidade Federal do Ceará.</p>
-        <p>Nosso objetivo é promover as equipes participantes dos programas de fomento a inovação da universidade</p>
-        <Link to="/showcase">
-          <Button block color='primary'>
-            Conhecer os projetos
-          </Button>
-        </Link>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = 'access' >
+
+        <
+        div className = 'access-image'
+        style = {
+            { backgroundImage: imageConfig } }
+        alt = 'Empreende UFC' >
+        <
+        div className = 'access-image-title' >
+        Portfólio de Startups <
+        /div>
+
+        <
+        /div> <
+        div className = 'access-form' > { < img src = { portIcon }
+            className = 'logo'
+            alt = 'logo' / > } <
+        h2 > Bem - vindo(a) ! < /h2> <
+        p > O Portfólio de Startups é uma iniciativa do Condomínio de Empreendedorismo da Universidade Federal do Ceará. < /p> <
+            p > Nosso objetivo é promover as equipes participantes dos programas de fomento a inovação da universidade < /p> <
+            Link to = "/showcase" >
+            <
+            Button block color = 'primary' >
+            Conhecer os projetos <
+            /Button> <
+            /Link> <
+            /div> <
+            /div>
+    );
 }
